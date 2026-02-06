19e43ef8879ac0d11471ad8c62655d1555115433
dongocbao19112006 <dongocbao19112006@gmail.com>
2026-02-06 22:53:24 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using inventory.Data;
+//using inventory.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +27,7 @@ namespace inventory
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BooksDB")));
+           // services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BooksDB")));
             
         }
 
@@ -53,3 +53,4 @@ namespace inventory
         }
     }
 }
+
